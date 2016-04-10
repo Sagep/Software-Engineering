1b6dd795e96f92177b2e349237172a09770752eb
SageP <sageporter34@gmail.com>
2016-04-09 20:19:08 -0600

    Fixed DB connection on a new PC. Created install package.

--- SoftwareEngineeringProject/SoftwareEngineeringProject/Program.cs
+++ SoftwareEngineeringProject/SoftwareEngineeringProject/Program.cs
@@ -14,6 +14,15 @@ namespace SoftwareEngineeringProject
         [STAThread]
         static void Main()
         {
+            string dblocations = "";
+
+            dblocations = System.IO.File.ReadAllText(@"DBlocation.txt");
+            dblocations = dblocations.Replace(@"\\", @"\");
+            //System.Windows.Forms.MessageBox.Show(dblocations);
+            //dblocations = "C:\\Users\\Sage\\Documents\\GitHub\\Software-Engineering\\SoftwareEngineeringProject\\SoftwareEngineeringProject\\obj\\Debug";
+            //System.Windows.Forms.MessageBox.Show(dblocations);
+
+            AppDomain.CurrentDomain.SetData("DataDirectory", ""+dblocations); 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
